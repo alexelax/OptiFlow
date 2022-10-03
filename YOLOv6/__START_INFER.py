e9696a9dea38445de086d84b1436ca232128a6b4
Rarder44 <lpozzi44@gmail.com>
2022-10-03 18:52:38 +0200

    fix script v6 fix gitignore

--- YOLOv6/__START_INFER.py
+++ YOLOv6/__START_INFER.py
@@ -1,54 +1,60 @@
 import tools.infer as infer
 import argparse
+import os 
 
-# Variables to set for parser
-# Model path for inference
-pathInference = './_infer/models/yolo6s.pt'
-# The path with the file to be evaluated
-pathEvalData = '../Resources/infer_data/traffic1.mp4'
-# Path of train file
-pathTrainSet = './_train/custom.yaml'
-# The image-size(h,w) in inference size (N.B. the size of the image should be the same of the train)
-imgSize = [640, 640]
-# Confidence threshold for inference
-threshold = 0.4
-# NMS IoU threshold for inference (Non-maximum Suppression)
-nms = 0.45
-# Maximal inferences per image
-maxInfsXImage = 1000
-# The type of the device to run the model: cpu, 0 (for GPU), 0,1,2,3,... (for multi-GPU)
-device = '0'
-# Path to save outputs
-outputDir = './_infer/output'
-# Experiment name, saved to outputDir/name
-name = 'inferExp'
-# Hide labels
-hideLabels = False
-# Hide confidences
-hideConfidences = False
+if __name__ == '__main__':       
+    if os.path.split(os.getcwd() )[1] != "YOLOv6":
+        os.chdir("YOLOv6")
+
+        
+    # Variables to set for parser
+    # Model path for inference
+    pathInference = './_infer/models/yolo6s.pt'
+    # The path with the file to be evaluated
+    pathEvalData = '../Resources/infer_data/traffic1.mp4'
+    # Path of train file
+    pathTrainSet = './_train/custom.yaml'
+    # The image-size(h,w) in inference size (N.B. the size of the image should be the same of the train)
+    imgSize = [640, 640]
+    # Confidence threshold for inference
+    threshold = 0.4
+    # NMS IoU threshold for inference (Non-maximum Suppression)
+    nms = 0.45
+    # Maximal inferences per image
+    maxInfsXImage = 1000
+    # The type of the device to run the model: cpu, 0 (for GPU), 0,1,2,3,... (for multi-GPU)
+    device = '0'
+    # Path to save outputs
+    outputDir = './_infer/output'
+    # Experiment name, saved to outputDir/name
+    name = 'inferExp'
+    # Hide labels
+    hideLabels = False
+    # Hide confidences
+    hideConfidences = False
 
 
-# Parser
-parser = argparse.ArgumentParser(description='YOLOv6 Inference', add_help=True)
-parser.add_argument('--weights', type=str, default=pathInference, help='model path(s) for inference.')
-parser.add_argument('--source', type=str, default=pathEvalData, help='the source path, e.g. image-file/dir.')
-parser.add_argument('--yaml', type=str, default=pathTrainSet, help='data yaml file.')
-parser.add_argument('--img-size', nargs='+', type=int, default=imgSize,
-                    help='the image-size(h,w) in inference size.')
-parser.add_argument('--conf-thres', type=float, default=threshold, help='confidence threshold for inference.')
-parser.add_argument('--iou-thres', type=float, default=nms, help='NMS IoU threshold for inference.')
-parser.add_argument('--max-det', type=int, default=maxInfsXImage, help='maximal inferences per image.')
-parser.add_argument('--device', default=device, help='device to run our model i.e. 0 or 0,1,2,3 or cpu.')
-parser.add_argument('--save-txt', action='store_true', help='save results to *.txt.')
-parser.add_argument('--save-img', action='store_false', help='save visuallized inference results.')
-parser.add_argument('--save-dir', type=str, help='directory to save predictions in. See --save-txt.')
-parser.add_argument('--view-img', action='store_true', help='show inference results')
-parser.add_argument('--classes', nargs='+', type=int, help='filter by classes, e.g. --classes 0, or --classes 0 2 3.')
-parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS.')
-parser.add_argument('--project', default=outputDir, help='save inference results to project/name.')
-parser.add_argument('--name', default=name, help='save inference results to project/name.')
-parser.add_argument('--hide-labels', default=hideLabels, action='store_true', help='hide labels.')
-parser.add_argument('--hide-conf', default=hideConfidences, action='store_true', help='hide confidences.')
-parser.add_argument('--half', action='store_true', help='whether to use FP16 half-precision inference.')
+    # Parser
+    parser = argparse.ArgumentParser(description='YOLOv6 Inference', add_help=True)
+    parser.add_argument('--weights', type=str, default=pathInference, help='model path(s) for inference.')
+    parser.add_argument('--source', type=str, default=pathEvalData, help='the source path, e.g. image-file/dir.')
+    parser.add_argument('--yaml', type=str, default=pathTrainSet, help='data yaml file.')
+    parser.add_argument('--img-size', nargs='+', type=int, default=imgSize,
+                        help='the image-size(h,w) in inference size.')
+    parser.add_argument('--conf-thres', type=float, default=threshold, help='confidence threshold for inference.')
+    parser.add_argument('--iou-thres', type=float, default=nms, help='NMS IoU threshold for inference.')
+    parser.add_argument('--max-det', type=int, default=maxInfsXImage, help='maximal inferences per image.')
+    parser.add_argument('--device', default=device, help='device to run our model i.e. 0 or 0,1,2,3 or cpu.')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt.')
+    parser.add_argument('--save-img', action='store_false', help='save visuallized inference results.')
+    parser.add_argument('--save-dir', type=str, help='directory to save predictions in. See --save-txt.')
+    parser.add_argument('--view-img', action='store_true', help='show inference results')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by classes, e.g. --classes 0, or --classes 0 2 3.')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS.')
+    parser.add_argument('--project', default=outputDir, help='save inference results to project/name.')
+    parser.add_argument('--name', default=name, help='save inference results to project/name.')
+    parser.add_argument('--hide-labels', default=hideLabels, action='store_true', help='hide labels.')
+    parser.add_argument('--hide-conf', default=hideConfidences, action='store_true', help='hide confidences.')
+    parser.add_argument('--half', action='store_true', help='whether to use FP16 half-precision inference.')
 
-infer.main(parser.parse_args())
+    infer.main(parser.parse_args())
--- YOLOv6/__START_TRAIN.py
+++ YOLOv6/__START_TRAIN.py
@@ -1,78 +1,85 @@
 import tools.train as train
 import argparse
+import os 
 
-# Variables to set for parser
-# Path of train file
-pathTrainSet = './_train/custom.yaml'
-# Config description file
-pathConfigFile = './configs/yolov6s.py'
-# Path to save outputs
-outputDir = './_train/output'
-# Experiment name, saved to outputDir/name
-name = 'trainExp'
-# Train image size (pixels)
-imgSize = 640
-# Number of samples (images) processed before the model is updated for all GPUs
-batchSize = 32
-# Number of complete passes through the training dataset
-epoches = 400
-# Number of workers simultaneously putting data into RAM (N.B. setting workers to number of cores is a good rule)
-workers = 8
-# The type of the device: cpu, 0 (for GPU), 0,1,2,3,... (for multi-GPU)
-device = '0'
-# The number of GPU's
-gpuCount = 0
-# After how many epochs does the model evaluate
-evalInterval = 20
-# Evaluating every epoch for last such epochs (can be jointly used with evalInterval)'
-heavyEvalRange = 50
-# Url used to set up distributed training
-distUrl = 'trn://'
-# DDP parameter (Distributed Data Parallel)
-localRank = -1
-# Resume the most recent training
-resume = False
-# Stop strong aug at last n epoch, neg value (-1) not stop
-stopAugLastNEpoch = 15
-# Save last n epoch even not best or last, neg value (-1) not save
-saveLastNEpoch = -1
-# If is present the teacher model path
-teacherModelPath = None
-# Number used to control the randomness of predictions
-temperature = 20
 
-# Parser
-parser = argparse.ArgumentParser(description='YOLOv6 Training', add_help=True)
-parser.add_argument('--data-path', default=pathTrainSet, type=str, help='path of train')
-parser.add_argument('--conf-file', default=pathConfigFile, type=str, help='experiments description file')
-parser.add_argument('--img-size', default=imgSize, type=int, help='train, val image size (pixels)')
-parser.add_argument('--batch-size', default=batchSize, type=int, help='total batch size for all GPUs')
-parser.add_argument('--epochs', default=epoches, type=int, help='number of total epochs to run')
-parser.add_argument('--workers', default=workers, type=int, help='number of data loading workers (default: 8)')
-parser.add_argument('--device', default=device, type=str, help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-parser.add_argument('--eval-interval', default=evalInterval, type=int, help='evaluate at every interval epochs')
-parser.add_argument('--eval-final-only', action='store_true', help='only evaluate at the final epoch')
-parser.add_argument('--heavy-eval-range', default=heavyEvalRange, type=int,
-                    help='evaluating every epoch for last such epochs (can be jointly used with --eval-interval)')
-parser.add_argument('--check-images', action='store_true', help='check images when initializing datasets')
-parser.add_argument('--check-labels', action='store_true', help='check label files when initializing datasets')
-parser.add_argument('--output-dir', default=outputDir, type=str, help='path to save outputs')
-parser.add_argument('--name', default=name, type=str, help='experiment name, saved to output_dir/name')
-parser.add_argument('--dist_url', default=distUrl, type=str, help='url used to set up distributed training')
-parser.add_argument('--gpu_count', type=int, default=gpuCount)
-parser.add_argument('--local_rank', type=int, default=localRank, help='DDP parameter')
-parser.add_argument('--resume', nargs='?', const=True, default=resume, help='resume the most recent training')
-parser.add_argument('--write_trainbatch_tb', action='store_true',
-                    help='write train_batch image to tensorboard once an epoch, may slightly slower train speed if open')
-parser.add_argument('--stop_aug_last_n_epoch', default=stopAugLastNEpoch, type=int,
-                    help='stop strong aug at last n epoch, neg value not stop, default 15')
-parser.add_argument('--save_ckpt_on_last_n_epoch', default=saveLastNEpoch, type=int,
-                    help='save last n epoch even not best or last, neg value not save')
-parser.add_argument('--distill', action='store_true', help='distill or not')
-parser.add_argument('--distill_feat', action='store_true', help='distill featmap or not')
-parser.add_argument('--quant', action='store_true', help='quant or not')
-parser.add_argument('--calib', action='store_true', help='run ptq')
-parser.add_argument('--teacher_model_path', type=str, default=teacherModelPath, help='teacher model path')
-parser.add_argument('--temperature', type=int, default=temperature, help='distill temperature')
+if __name__ == '__main__':       
+    if os.path.split(os.getcwd() )[1] != "YOLOv6":
+        os.chdir("YOLOv6")
 
-train.main(parser.parse_args())
+
+    # Variables to set for parser
+    # Path of train file
+    pathTrainSet = './_train/custom.yaml'
+    # Config description file
+    pathConfigFile = './configs/yolov6s.py'
+    # Path to save outputs
+    outputDir = './_train/output'
+    # Experiment name, saved to outputDir/name
+    name = 'trainExp'
+    # Train image size (pixels)
+    imgSize = 640
+    # Number of samples (images) processed before the model is updated for all GPUs
+    batchSize = 32
+    # Number of complete passes through the training dataset
+    epoches = 400
+    # Number of workers simultaneously putting data into RAM (N.B. setting workers to number of cores is a good rule)
+    workers = 8
+    # The type of the device: cpu, 0 (for GPU), 0,1,2,3,... (for multi-GPU)
+    device = '0'
+    # The number of GPU's
+    gpuCount = 0
+    # After how many epochs does the model evaluate
+    evalInterval = 20
+    # Evaluating every epoch for last such epochs (can be jointly used with evalInterval)'
+    heavyEvalRange = 50
+    # Url used to set up distributed training
+    distUrl = 'trn://'
+    # DDP parameter (Distributed Data Parallel)
+    localRank = -1
+    # Resume the most recent training
+    resume = False
+    # Stop strong aug at last n epoch, neg value (-1) not stop
+    stopAugLastNEpoch = 15
+    # Save last n epoch even not best or last, neg value (-1) not save
+    saveLastNEpoch = -1
+    # If is present the teacher model path
+    teacherModelPath = None
+    # Number used to control the randomness of predictions
+    temperature = 20
+
+    # Parser
+    parser = argparse.ArgumentParser(description='YOLOv6 Training', add_help=True)
+    parser.add_argument('--data-path', default=pathTrainSet, type=str, help='path of train')
+    parser.add_argument('--conf-file', default=pathConfigFile, type=str, help='experiments description file')
+    parser.add_argument('--img-size', default=imgSize, type=int, help='train, val image size (pixels)')
+    parser.add_argument('--batch-size', default=batchSize, type=int, help='total batch size for all GPUs')
+    parser.add_argument('--epochs', default=epoches, type=int, help='number of total epochs to run')
+    parser.add_argument('--workers', default=workers, type=int, help='number of data loading workers (default: 8)')
+    parser.add_argument('--device', default=device, type=str, help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--eval-interval', default=evalInterval, type=int, help='evaluate at every interval epochs')
+    parser.add_argument('--eval-final-only', action='store_true', help='only evaluate at the final epoch')
+    parser.add_argument('--heavy-eval-range', default=heavyEvalRange, type=int,
+                        help='evaluating every epoch for last such epochs (can be jointly used with --eval-interval)')
+    parser.add_argument('--check-images', action='store_true', help='check images when initializing datasets')
+    parser.add_argument('--check-labels', action='store_true', help='check label files when initializing datasets')
+    parser.add_argument('--output-dir', default=outputDir, type=str, help='path to save outputs')
+    parser.add_argument('--name', default=name, type=str, help='experiment name, saved to output_dir/name')
+    parser.add_argument('--dist_url', default=distUrl, type=str, help='url used to set up distributed training')
+    parser.add_argument('--gpu_count', type=int, default=gpuCount)
+    parser.add_argument('--local_rank', type=int, default=localRank, help='DDP parameter')
+    parser.add_argument('--resume', nargs='?', const=True, default=resume, help='resume the most recent training')
+    parser.add_argument('--write_trainbatch_tb', action='store_true',
+                        help='write train_batch image to tensorboard once an epoch, may slightly slower train speed if open')
+    parser.add_argument('--stop_aug_last_n_epoch', default=stopAugLastNEpoch, type=int,
+                        help='stop strong aug at last n epoch, neg value not stop, default 15')
+    parser.add_argument('--save_ckpt_on_last_n_epoch', default=saveLastNEpoch, type=int,
+                        help='save last n epoch even not best or last, neg value not save')
+    parser.add_argument('--distill', action='store_true', help='distill or not')
+    parser.add_argument('--distill_feat', action='store_true', help='distill featmap or not')
+    parser.add_argument('--quant', action='store_true', help='quant or not')
+    parser.add_argument('--calib', action='store_true', help='run ptq')
+    parser.add_argument('--teacher_model_path', type=str, default=teacherModelPath, help='teacher model path')
+    parser.add_argument('--temperature', type=int, default=temperature, help='distill temperature')
+
+    train.main(parser.parse_args())
