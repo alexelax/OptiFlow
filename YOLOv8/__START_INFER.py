099990cf7e4cbeb853b7a79edd9da6ce5cd7c45a
Rarder44 <lpozzi44@gmail.com>
2023-01-11 17:13:42 +0100

    Aggiunto YOLOv8

---
+++ YOLOv8/__START_INFER.py
@@ -0,0 +1,11 @@
+#https://github.com/ultralytics/ultralytics
+from ultralytics import YOLO
+import cv2
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+results = model("../Resources/infer_data/traffic1.mp4",show=True)  # predict on an image
+#results = model("0",show=True)  # webcam
+
