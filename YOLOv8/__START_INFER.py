a6fb7f13c2f5de7101810cedd547ad25c64e9df1
Rarder44 <lpozzi44@gmail.com>
2023-01-19 21:06:13 +0100

    aggiunto nuovo PT v5 trainato --img 640 --batch 128 --epochs 3000

--- YOLOv8/__START_INFER.py
+++ YOLOv8/__START_INFER.py
@@ -3,9 +3,9 @@ from ultralytics import YOLO
 import cv2
 
 # Load a model
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("_infer/best_n_100.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-results = model("../Resources/infer_data/traffic1.mp4",show=True,device="cpu")  # predict on an image
+results = model("../Resources/infer_data/traffic1.mp4",show=True)  # predict on an image
 #results = model("0",show=True)  # webcam
 
---
+++ _START_TRAFFIC_MONITORING.py
@@ -0,0 +1,5 @@
+
+#aprire 4 flussi video
+#ciascun frame unirlo in un unico flusso video
+#analizzare il frame con YOLO
+#visualizzare l'output con cv2
